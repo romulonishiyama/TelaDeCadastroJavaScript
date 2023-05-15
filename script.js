8e313cd90f9c2ee12d0293afd059c42ee3f919cb
romulo nishiyama <romulonishiyama@gmail.com>
2023-05-15 16:57:24 -0300

    2

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 async function buscaEndereco() {
+    try {
+        var consultaCEP = await fetch('https://viacep.com.br/ws/72300531/json/');
+        var consultaCEPConvertida = await consultaCEP.json();
+        if(!consultaCEPConvertida){
+            throw Error("CEP não existente!")
+        }
+        console.log(consultaCEPConvertida);
 
-    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
-    var consultaCEPConvertida = await consultaCEP.json();
-    console.log(consultaCEPConvertida);
+    } catch (erro) {
+        console.log(erro);
+    }
+
 };
 
 // .then(resposta => resposta.json())
